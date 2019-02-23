3b73faa504012fb62c045530fd1365170b52ebce
rfarhanian <rf.tech@icloud.com>
2019-02-22 17:01:03 -0800

    Three b with example 1 classifiers

--- src/hw2/Main.py
+++ src/hw2/Main.py
@@ -1,7 +1,6 @@
 from hw2.ClaimParser import ClaimParser
 from hw2.One import One
 from hw2.Three import Three
-from hw2.Two import Two
 
 parser = ClaimParser("./input/claim.sample.csv")
 jay_starting_claims_context = One.count_column_values(parser.get_rows(), column_name='ProcedureCode', prefix='J')
@@ -10,12 +9,12 @@ print("One B result:", One.in_network_price_paid(parser.get_rows()))
 provider_sorted_jay_codes = One.top_jay_code_based_on_providers(jay_starting_claims_context)
 print("One C result:", provider_sorted_jay_codes.get_top_procedure_codes(5))
 
-two = Two('./result/', 'HW2-Result')
-two.a(parser.get_rows(), jay_starting_claims_context.get_codes())
-two.b()
-two.c()
-two.finalize()
+# two = Two('./result/', 'HW2-Result')
+# two.a(parser.get_rows(), jay_starting_claims_context.get_codes())
+# two.b()
+# two.c()
+# two.finalize()
 
-three = Three(provider_sorted_jay_codes)
+three = Three(provider_sorted_jay_codes, False)
 print('Three A: What percentage of J-code claim lines were unpaid?', three.a())
-print('Three B:', three.b())
+three.b()
--- src/hw2/Three.py
+++ src/hw2/Three.py
@@ -1,19 +1,13 @@
-from itertools import product
-
 import numpy as np
 from sklearn import preprocessing
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import KFold
-from sklearn.neighbors import KNeighborsClassifier
 
 from hw2.domain.ProviderSortedJayCodes import ProviderSortedJayCodes
+from hw2.util.ClassifiersExecutor import ClassifiersExecutor
 
 
 class Three:
 
-    def __init__(self, provider_sorted_jay_codes: ProviderSortedJayCodes):
+    def __init__(self, provider_sorted_jay_codes: ProviderSortedJayCodes, use_subset):
         all_claim_codes = provider_sorted_jay_codes.get_jay_codes()
         unpaid_claims_mask = (all_claim_codes['ProviderPaymentAmount'] == 0)
         paid_claims_mask = (all_claim_codes['ProviderPaymentAmount'] > 0)
@@ -97,7 +91,6 @@ class Three:
         # here we just combine rows
         self.all_claims_w_L = np.concatenate((self.unpaid_claims_L, self.paid_claims_L), axis=0)
         self.init_model_data()
-        self.clfsAccuracyDict = {}
 
     def a(self):
         # What percentage of J-code claim lines were unpaid?
@@ -106,20 +99,19 @@ class Three:
     def init_model_data(self):
         # Create a model to predict when a J-code is unpaid. Explain why you choose the modeling approach.
 
-        # We need to shuffle the rows before using classifers in sklearn
+        # 1- We need to shuffle the rows
         self.all_claims_w_L.dtype.names
         # Apply the random shuffle
         np.random.shuffle(self.all_claims_w_L)
-
         self.all_claims_w_L.dtype.names
 
         label = 'IsUnpaid'
-        # I just removed columns that are unique or have significant missings or redundant in our model
+        # I just removed columns that are unique or have too many missing values or redundant in our model
         # Also by running a Random Forest Feature Importance selection, I found columns have the lowest importance
         # and filtered them out.
         numeric_features = ['ClaimNumber', 'ClaimLineNumber', 'ClaimChargeAmount', 'SubscriberIndex', 'SubgroupIndex', ]
 
-        categorical_features = [
+        categoricals = [
             'ProviderID', 'LineOfBusinessID',  # 'RevenueCode',
             'ServiceCode',  # 'PlaceOfServiceCode', 'ProcedureCode', 'DiagnosisCode',  # 'DenialReasonCode',
             # 'PriceIndex', 'InOutOfNetwork', 'ReferenceIndex',
@@ -128,92 +120,28 @@ class Three:
             'NetworkID',
             'AgreementID', 'ClaimType']
 
-        # convert features to list, then to np.array
+
         # separate categorical and numeric features
-        Mcat = np.array(self.all_claims_w_L[categorical_features].tolist())
-        Mnum = np.array(self.all_claims_w_L[numeric_features].tolist())
+        categorical_features = np.array(
+            self.all_claims_w_L[categoricals].tolist())  # convert features to list, then to np.array
+        numerical_features = np.array(
+            self.all_claims_w_L[numeric_features].tolist())  # convert features to list, then to np.array
         L = np.array(self.all_claims_w_L[label].tolist())
 
-        # Run the Label encoder
-        le = preprocessing.LabelEncoder()
-        for i in range(len(categorical_features)):
-            Mcat[:, i] = le.fit_transform(Mcat[:, i])
+        label_encoder = preprocessing.LabelEncoder()
+        for i in range(len(categoricals)):
+            categorical_features[:, i] = label_encoder.fit_transform(categorical_features[:, i])
 
-        # Run the OneHotEncoder
         OneHotEncoder = preprocessing.OneHotEncoder(sparse=False)
-        Mcat = OneHotEncoder.fit_transform(Mcat)
+        categorical_features = OneHotEncoder.fit_transform(categorical_features)
 
-        # For start and to avoid getting memory error, I subset the data
-
-        # Mcat_subset = Mcat[0:500]
-        # Mcat_subset.shape
-        #
-        # Mnum_subset = Mnum[0:500]
-        # Mnum_subset.shape
-
-        M = np.concatenate((Mnum, Mcat), axis=1)  # Concatenate the columns
+        M = np.concatenate((numerical_features, categorical_features), axis=1)  # Concatenate the columns
         L = self.all_claims_w_L[label].astype(int)
-
         M.shape
         L.shape
-
         np.corrcoef(M)
-
         self.data = (M, L, 5)
 
-        # *************************************************************************
-        self.all_models = [RandomForestClassifier, KNeighborsClassifier, LogisticRegression, GradientBoostingClassifier]
-
-        self.hyper_param_mapping = {
-            'RandomForestClassifier': {"min_samples_split": [2], "n_jobs": [-1], "n_estimators": [10, 100, 300]},
-            'LogisticRegression': {"tol": [0.1], "C": [0.001], "penalty": ["l1"]},
-            'KNeighborsClassifier': {"n_neighbors": [4, 6], "n_jobs": [-1], "leaf_size": [20], "algorithm": ["auto"]},
-            'GradientBoostingClassifier': {"min_samples_split": [4], "min_weight_fraction_leaf": [0.3]}
-        }
-
-        return None
-
-    def run(self, clfs, data, clf_hyper={}):
-        M, L, n_folds = data
-        kf = KFold(n_splits=n_folds)
-        result = {}
-
-        for ids, (train_index, test_index) in enumerate(kf.split(M, L)):
-            clf = clfs(**clf_hyper)
-            clf.fit(M[train_index], L[train_index])
-            pred = clf.predict(M[test_index])
-            result[ids] = {'clf': clf, 'accuracy': accuracy_score(L[test_index], pred)}
-        return result
-
-    def init_accuracy_mapping(self, Output):
-        # Create a dictionary containing the result of accuracy for each set
-        for key in Output:
-            k1 = Output[key]['clf']
-            v1 = Output[key]['accuracy']
-            k1Test = str(k1)
-
-            k1Test = k1Test.replace('                    ', '  ')
-            k1Test = k1Test.replace('               ', '  ')
-            if k1Test in self.clfsAccuracyDict:
-                self.clfsAccuracyDict[k1Test].append(v1)
-            else:
-                self.clfsAccuracyDict[k1Test] = [v1]
-
-    def run_all(self):
-        # this function will get the results of each hyper parameter combinations and use for run function
-        for clf in self.all_models:
-            # to check if values in ModelsList are in ParamDict
-            clf_str = str(clf)
-            for key, value in self.hyper_param_mapping.items():  # go through the inner dictionary of hyper parameters
-                if key in clf_str:
-                    # to do all the matching key and values
-                    k2, v2 = zip(*value.items())
-                    for values in product(*v2):  # for the values in the inner dictionary, get their unique combinations
-                        hyperSet = dict(zip(k2, values))  # create a dictionary from their values
-                        Output = self.run(clf, self.data, hyperSet)
-                        self.init_accuracy_mapping(Output)
-                        # print(Output)
-
     def b(self):
-        self.run_all()
-        return self.clfsAccuracyDict
+        executor = ClassifiersExecutor()
+        executor.execute(self.data)
